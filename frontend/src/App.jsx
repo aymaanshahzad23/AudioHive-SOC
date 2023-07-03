8a7b177467b2bf7b589298d47abe4e514b268188
Aymaan Shahzad <22b1520@iitb.ac.in>
2023-07-04 00:56:14 +0530

    Updated App.jsx ~Aymaan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Clubs from "./pages/Clubs.jsx";
+import "./App.css"
+import Home from "./pages/Home.jsx"
 import Error404 from "./pages/Error404.jsx";
-
+import PlanLayout from "./pages/Layouts/PlanLayout.jsx";
 
 export default function App() {
   return (
+    <PlanLayout>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/club" element={<Clubs/>}/>
         <Route path="*" element={<Error404/>}/>
       </Routes>
     </BrowserRouter>
+    </PlanLayout>
   )
-}
+} 
