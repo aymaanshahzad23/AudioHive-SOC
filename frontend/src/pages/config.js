7aed4e364d438d9f3dca0c193f740ac4fbb9bc9a
Aymaan Shahzad <aymaanshahzad23@gmail.com>
2023-07-13 11:54:29 +0530

    SRC updated

--- frontend/src/pages/config.js
+++ frontend/src/pages/config.js
@@ -1,37 +1,37 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider} from "firebase/auth";
-import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+import {getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'; 
+
 const firebaseConfig = {
   apiKey: "AIzaSyBUtSKT96HgJ7hnIhqFRL5G3_-CDhwpNos",
   authDomain: "soc-audiohive.firebaseapp.com",
+  databaseURL: "https://soc-audiohive-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "soc-audiohive",
   storageBucket: "soc-audiohive.appspot.com",
   messagingSenderId: "881472567929",
   appId: "1:881472567929:web:e76a498c93a8f79737b2a8"
-};
+}
 
-const firebaseApp = initializeApp(firebaseConfig);
+const FirebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
-  prompt:"select_account",
-});
-
 export const auth = getAuth()
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider).then((result)=>{
+console.log(auth, provider)
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+.then((result)=>{
   const username = result.user.displayName
   const email = result.user.email
   const profilePic = result.user.photoURL
-
+  
   localStorage.setItem("name", username)
   localStorage.setItem("email", email)
   localStorage.setItem("profilePic", profilePic)
 });
-
-export const db = getFirestore();
+export const db = getFirestore(FirebaseApp);
 
 
 export const createUserDocumentFromAuth = async (userAuth)=>{
+  console.log(userAuth)
   const userDocRef = doc(db, 'users', userAuth.uid);
   const userSnapshot = await getDoc(userDocRef)
  
