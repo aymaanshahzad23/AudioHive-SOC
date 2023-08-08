fdbccaf3729f686d545f9895c1c624c8dc9d521d
Aymaan Shahzad <aymaanshahzad23@gmail.com>
2023-08-08 19:15:34 +0530

    DesignUpdated

--- frontend/src/components/Lobby.jsx
+++ frontend/src/components/Lobby.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSocket } from "../context/SocketProvider";
-import "../styles/lobby.css"
+import "../styles/lobby.css";
+
 const LobbyScreen = () => {
   const [email, setEmail] = useState("");
   const [room, setRoom] = useState("");
@@ -17,6 +18,10 @@ const LobbyScreen = () => {
         alert("Please fill in both Display Name and Room Number");
         return;
       }
+
+      // Store values in localStorage
+      localStorage.setItem("displayName", email);
+      localStorage.setItem("room", room);
 
       socket.emit("room:join", { email, room });
     },
@@ -38,34 +43,42 @@ const LobbyScreen = () => {
     };
   }, [socket, handleJoinRoom]);
 
+  useEffect(() => {
+    // Load stored values from localStorage
+    const storedEmail = localStorage.getItem("displayName");
+    const storedRoom = localStorage.getItem("room");
+    // if (storedEmail) {
+    //   setEmail(storedEmail);
+    // }
+    // if (storedRoom) {
+    //   setRoom(storedRoom);
+    // }
+  }, []);
+
   return (
     <>
       {/* <h1>Lobby</h1> */}
-        <form onSubmit={handleSubmitForm}>
-      <div className="formContainer">
+      <form onSubmit={handleSubmitForm}>
+        <div className="formContainer">
           <div className="formName">
-            <label className="name" htmlFor="email">Display Name</label>
-            <input
-              type="text"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-            <br />
-          <div className="formRoom">
-            <label className="room" htmlFor="room">Room Number</label>
-            <input
-              type="text"
-              id="room"
-              value={room}
-              onChange={(e) => setRoom(e.target.value)}
-            />
+            {/* <label className="name" htmlFor="email">
+              Display Name
+            </label> */}
+            <input placeholder="ChatRoom Name" autoComplete="off" type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
           </div>
           <br />
-      </div>
-          <button className="formSubmit" type="submit">Join Room</button>
-        </form>
+          <div className="formRoom">
+            {/* <label className="room" htmlFor="room">
+              Room Number
+            </label> */}
+            <input placeholder="Room ID" autoComplete="off" type="text" id="room" value={room} onChange={(e) => setRoom(e.target.value)}/>
+          </div>
+          <br />
+        </div>
+        <button className="formSubmit" type="submit">
+          Join Room
+        </button>
+      </form>
     </>
   );
 };
